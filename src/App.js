33f7a4037b90b398f9f1a41b89356968fa7ad538
Munhjinn <munhjinmunhjin994@gmail.com>
2023-11-07 21:42:27 +0800

    p

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 
+import { useState } from 'react';
 import './App.css';
 import {Auth} from "./components/auth"
+import{db} from  "./config/firebase"
 
 function App()  {
+    const [movieList, setMovieList] = useState([])
+
+    const getMovieList = () => {
+
+        
+    }
     return (
         <div className="App">
             <Auth/>
 
-       
+
         </div>
     );
         
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
+import {getFirestore} from 'firebase/firestore'
 
 
 
@@ -18,3 +19,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+
+export const db = getFirestore(app)
