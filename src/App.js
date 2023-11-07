9323fd1eae5580d48efca74c46ea9c9c2d674cd0
Munhjinn <munhjinmunhjin994@gmail.com>
2023-11-08 00:09:44 +0800

    w

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-
-import { useState } from 'react';
+import React from 'react';
+import { useState,useEffect } from 'react';
 import './App.css';
 import {Auth} from "./components/auth"
 import{db} from  "./config/firebase"
+import{getDocs, collection}  from 'firebase/firestore'
 
 function App()  {
     const [movieList, setMovieList] = useState([])
+    const moviesCollectionRef = collection(db, "Movie")
 
-    const getMovieList = () => {
+    useEffect(() => {
+    const getMovieList = async () => {
+        //READ THE DATA
+        //SET THE MOVIE LIST
+        try{
+        const data = await getDocs(moviesCollectionRef);
+        const filteredData = data.docs.map((doc) => ({...doc.data(), id: doc.id,}))
+        setMovieList(filteredData)
+        } catch (err) {
+            console.error(err);
+        }
 
-        
     }
+    getMovieList();
+}, [])
+
     return (
         <div className="App">
             <Auth/>
+            
+            <div>
 
+            { movieList.map((Movie)=> (
+                <div>
+                    <h1>{Movie.title}</h1>
+                    <p> Date: {Movie.releaseDate} </p>
+                </div>
+            ))}
 
         </div>
+        </div>
+
     );
         
 
