290364ecee5ede5862c5b9d7395e96735f070ef1
Munhjinn <munhjinmunhjin994@gmail.com>
2023-11-14 19:10:26 +0800

    h

--- src/App.js
+++ src/App.js
@@ -3,26 +3,49 @@ import { useState,useEffect } from 'react';
 import './App.css';
 import {Auth} from "./components/auth"
 import{db} from  "./config/firebase"
-import{getDocs, collection,addDoc,deleteDoc}  from 'firebase/firestore'
+import{getDocs, collection,addDoc,deleteDoc,doc}  from 'firebase/firestore'
 
 function App()  {
     const [movieList, setMovieList] = useState([])
 
     // NEw movie staties
-    const [newMovieTitle, setNewMovieTitle] = useState("")
-    const [newReleaseDate, setNewReleaseDate] = useState(0)
-    const [isnewMovieAward, setIsNewMovieAward] = useState(false)
+    const [newMovieTitle, setNewMovieTitle] = useState("");
+    const [newReleaseDate, setNewReleaseDate] = useState(0);
+    const [isNewMovieOscar, setIsNewMovieOscar] = useState(false);
 
 
-    const moviesCollectionRef = collection(db, "Movie")
+    const moviesCollectionRef = collection(db, "movies")
 
-    useEffect(() => {
+
+const onSubmitMovie = async () => {
+    try{
+
+    await addDoc(moviesCollectionRef,
+        {
+        title: newMovieTitle, 
+        releaseDate: newReleaseDate, 
+        receivedAnOscar: isNewMovieOscar,
+
+    }) ;
+
+    const deleteMovie = async (id) => {
+        const movieDoc = doc(db, "movies", id)
+        await deleteDoc(movieDoc);
+    };
+            
+} catch(err) {
+    console.error(err)
+}
+
+}
+
+useEffect(() => { 
     const getMovieList = async () => {
         //READ THE DATA
         //SET THE MOVIE LIST
         try{
         const data = await getDocs(moviesCollectionRef);
-        const filteredData = data.docs.map((doc) => ({...doc.data(), id: doc.id,}))
+        const filteredData = data.docs.map((doc) => ({...doc.data(), id: doc.id,}));
         setMovieList(filteredData)
         } catch (err) {
             console.error(err);
@@ -32,27 +55,6 @@ function App()  {
     getMovieList();
     
 }, [])
-
-const onSubmitMovie = async () => {
-    try{
-
-    await addDoc(moviesCollectionRef,{
-        title: newMovieTitle, 
-        releaseDate: newReleaseDate,
-        award: isnewMovieAward,
-
-    }) ;
-
-    const deleteMovie = async (id) => {
-        const movieDoc = doc(db, "Movie", id)
-        await deleteDoc();
-    };
-            
-} catch(err) {
-    console.error(err)
-}
-
-}
 
     return (
         <div className="App">
@@ -68,22 +70,22 @@ const onSubmitMovie = async () => {
                 <input 
                     type="checkbox "
                     // checked={isnewMovieAward} 
-                    onChange={(e) => setIsNewMovieAward(e.target.checked)}  />
-                <label>Received an Award</label>
+                    onChange={(e) => setIsNewMovieOscar(e.target.checked)}  />
+                <label>Received an Oscar</label>
                 <button onCLick={onSubmitMovie} > Submit Movie.. </button>
 
                 
             </div>
             
             <div>
-            { movieList.map((Movie)=> (
+            { movieList.map((movie)=> (
                 <div>
-                    <h1 style={{color: Movie.receivedAnAward ? "blue" : "red"}} >{Movie.title}</h1>
-                    <p> Date: {Movie.releaseDate} </p>
+                    <h1 style={{color: movie.receivedAnOscar ? "blue" : "red"}} >{movie.title}</h1>
+                    <p> Date: {movie.releaseDate} </p>
                 </div>
             ))}
 
-            <button onClick={() => deleteMovie(Movie.id)}> Delete Movie</button>
+            <button onClick={() => deleteMovie(movie.id)}> Delete Movie</button>
  
         </div>
         </div>
